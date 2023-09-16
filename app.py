9521ab6b45b49faf36c16deb7605ae6e72934c39
Daily <nikoletta.matsur@tecnico.ulisboa.pt>
2023-09-16 13:24:47 +0100

    Implement endpoint to get historical data from a date

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask, request, Response
+from flask import Flask, request
 import csv
 import json
-from dtos import SensorLocationResponse
+from dtos import SensorLocationResponse, SensorTrafficData
 import logging
+import os
 
 app = Flask(__name__)
 
-sensor_location_data_path = "./sensors_data/SensorLocationMetadata.csv"
 logging.getLogger().setLevel(logging.INFO)
+
+sensor_location_data_path = "./sensors_data/SensorLocationMetadata.csv"
+sensor_traffic_data_path = "./data/12_06_to_24_06"
 
 @app.route('/prediction', methods=['GET'])
 def getPrediction():
@@ -18,8 +21,17 @@ def getPrediction():
 
 @app.route('/sensor/search', methods=['GET'])
 def getSensorLocation():
+    """
+    Get Sensor information regarding its localization
+    :query params: name and street
+    Example:
+        http://127.0.0.1:5000/sensor/search?name=enne
+    """
     name_search = request.args.get('name')
     street_search = request.args.get('street')
+
+    if not name_search and not street_search:
+        return json.JSONEncoder().encode({})
 
     with open(sensor_location_data_path, "r") as opened_file:
         sensor_location_data = csv.reader(opened_file, delimiter=";")
@@ -27,13 +39,65 @@ def getSensorLocation():
         for row in sensor_location_data:
             if name_search and name_search.lower() in row[5].lower() : # Name
                 logging.info('Sensor name searched %s, match found %s', name_search, row[5])
-                return SensorLocationResponse(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8]).toJson()
+                return SensorLocationResponse(
+                    row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8]
+                    ).toDictionary()
             
             elif street_search and street_search.lower() in row[8].lower() : # Street
                 logging.info('Sensor street searched %s, match found %s', street_search, row[8])
-                return SensorLocationResponse(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8]).toJson()
+                return SensorLocationResponse(
+                    row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8]
+                    ).toDictionary()
             
         logging.info('Sensor searched %s, no match found', name_search if name_search else street_search)
         return json.JSONEncoder().encode({})
+    
+@app.route('/historical-traffic-data/search', methods=['GET'])
+def getHistoricalTrafficData():
+    """
+    Get historical traffic information by all sensors for a specified date
+    :query params: search by date in the format '2023-06-12T18:32:00.000000Z' - any substring
+    Will scan through files searching for the date - after it finds one, it will scan rows and files
+      until it does not find one in a row - then it stops, for efficiency
+    Assumptions: 
+        - Data is ordered by date
+        - Each csv is a sensor
+    Example:
+        http://127.0.0.1:5000/historical-traffic-data/search?date=2023-06-12T18:26:00.000000Z
+    """
+    date_search = request.args.get('date')
+    if not date_search:
+        return json.JSONEncoder().encode({})
+
+    historical_data_files = os.listdir(sensor_traffic_data_path)
+    results = list()
+    
+    for file in historical_data_files:
+        file_path = sensor_traffic_data_path + "/" + file
+
+        with open(file_path, "r") as opened_file:
+            sensor_traffic_data = csv.reader(opened_file, delimiter=",")
+
+            for row in sensor_traffic_data:
+
+                if date_search in row[2]:
+                    results.append(
+                        SensorTrafficData(
+                            row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8]
+                        ).toDictionary()
+                    )
+                    break  # time already found
+        
+        if not len(results) > 0:
+            break
+
+    return { "count": len(results), "results": results }
+
+            
+        
+            
+
+
+    
     
 
--- dtos.py
+++ dtos.py
@@ -12,9 +12,8 @@ class SensorLocationResponse:
         self.direction = direction
         self.street = street
 
-    def toJson(self):
-        return json.JSONEncoder().encode(
-            {
+    def toDictionary(self):
+        return {
                 "id": self.id,
                 "lane": self.lane,
                 "orientation": self.orientation,
@@ -25,6 +24,30 @@ class SensorLocationResponse:
                 "direction": self.direction,
                 "street": self.street
              }
-        ) 
+    
 
+class SensorTrafficData:
+    def __init__(self, record_id, sensor_id, timestamp, car_flow, lorry_flow, any_flow, car_speed, lorry_speed, any_speed):
+        self.record_id = record_id
+        self.sensor_id = sensor_id
+        self.timestamp = timestamp
+        self.car_flow = car_flow
+        self.lorry_flow = lorry_flow
+        self.any_flow = any_flow
+        self.car_speed = car_speed
+        self.lorry_speed = lorry_speed
+        self.any_speed = any_speed
+
+    def toDictionary(self):
+        return {
+                "record_id": self.record_id,
+                "sensor_id": self.sensor_id,
+                "timestamp": self.timestamp,
+                "car_flow": self.car_flow,
+                "lorry_flow": self.lorry_flow,
+                "any_flow": self.any_flow,
+                "car_speed": self.car_speed,
+                "lorry_speed": self.lorry_speed,
+                "any_speed": self.any_speed
+            }
 
